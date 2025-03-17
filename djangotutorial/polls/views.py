d1bde3d8584a21e1ad0fcd591b3e2fb58ca8d090
TommiTH <TommiTHyyrylainen@gmail.com>
2025-03-17 22:02:15 +0200

    added registration functionality and the first flaw

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -3,9 +3,9 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.shortcuts import redirect
 
 from .models import Choice, Question
-
 
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
@@ -15,7 +15,6 @@ class IndexView(generic.ListView):
         """Return the last five published questions."""
         return Question.objects.order_by("-pub_date")[:5]
 
-
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
@@ -24,9 +23,14 @@ class DetailView(generic.DetailView):
 class ResultsView(generic.DetailView):
     model = Question
     template_name = "polls/results.html"
+    
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
+    print("user is authenticated:" , request.user.is_authenticated)
+    if not request.user.is_authenticated:
+        redirect("/polls")
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
     try:
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
